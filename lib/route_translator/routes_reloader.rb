9a49100b448ab7a300e7daff5f5b1ed2f3c44312
Enric Lluelles <enric@lluell.es>
2012-09-21 16:18:22 +0200

    Only open the RoutesReloader class if it's defined

--- lib/route_translator/routes_reloader.rb
+++ lib/route_translator/routes_reloader.rb
@@ -2,18 +2,20 @@ require "rails"
 
 module Rails
   class Application
-    class RoutesReloader
-      alias :__reload__! :reload!
+    if defined?(RoutesReloader)
+      class RoutesReloader
+        alias :__reload__! :reload!
 
-      def reload!
-        result = __reload__!
+        def reload!
+          result = __reload__!
 
-        route_sets.each do |routes|
-          routes.default_locale = I18n.default_locale
-          routes.translate_from_file
+          route_sets.each do |routes|
+            routes.default_locale = I18n.default_locale
+            routes.translate_from_file
+          end
+
+          result
         end
-
-        result
       end
     end
   end
